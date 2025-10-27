e70a99cc534a77ecab27a21696eaf371ebe400b1
Nedzhib Ahmed <nedjib500@gmail.com>
2025-10-27 17:06:29 +0100

    fix: generate client sctipt alternative python command

--- scripts/generate-client.sh
+++ scripts/generate-client.sh
@@ -4,6 +4,8 @@ set -e
 set -x
 
 cd backend
+# linux version
+# uv run python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ../openapi.json
 python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ../openapi.json
 cd ..
 mv openapi.json frontend/
